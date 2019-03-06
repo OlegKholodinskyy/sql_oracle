e6b771c6c0ff6bd1fcf14b7da4d0192e7d94830b
Oleg_hos <ooleguner@gmail.com>
2019-03-07 00:25:44 +0200

    create table

--- IdeaProjects/relational-db/src/lesson2/Demo.java
+++ IdeaProjects/relational-db/src/lesson2/Demo.java
@@ -1,12 +1,14 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         //java
         // create class;
         // create object with some values
         //use it;
-
+Order order = new Order(1001,"tool2212",140, new Date(), new Date());
 
         //db
         // create table with columns;
